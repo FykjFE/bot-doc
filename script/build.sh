4c08854572af24fea00a859ba83d2aab07ad339f
zzfn <chenc@fengyuntec.com>
2020-09-27 02:16:42 +0800

    :ambulance:

--- script/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-npm i
-npm run build
-tar -zcvf build.tar.gz ./build
--- script/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-name=bot.zzfzzf.com
-
-cd /www/wwwroot/$name
-rm -rf build/*
-tar -zxvf build.tar.gz
-rm -rf build.tar.gz
